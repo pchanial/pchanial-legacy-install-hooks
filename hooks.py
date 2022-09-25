0edcea1d28fdfb51830e02102f07488d3402ae65
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-09-25 23:32:54 +0200

    Properly handle environment variables FFLAGS, F77FLAGS and F90FLAGS.

--- hooks.py
+++ hooks.py
@@ -3,6 +3,7 @@
 import os
 import sys
 from pathlib import Path
+from typing import Literal
 
 # we disable setuptools sdist see numpy github issue #7127
 if 'sdist' not in sys.argv:
@@ -16,7 +17,7 @@ from numpy.distutils.command.build_src import build_src
 from numpy.distutils.command.sdist import sdist
 from distutils.cmd import Command
 from numpy.distutils.exec_command import find_executable
-from numpy.distutils.fcompiler import new_fcompiler
+from numpy.distutils.fcompiler import FCompiler, new_fcompiler
 from numpy.distutils.fcompiler.gnu import Gnu95FCompiler
 from numpy.distutils.fcompiler.intel import IntelEM64TFCompiler
 from numpy.distutils.misc_util import f90_ext_match, has_f_sources
@@ -28,10 +29,10 @@ F77_OPENMP = True
 F90_OPENMP = True
 F77_COMPILE_ARGS_GFORTRAN = []
 F77_COMPILE_DEBUG_GFORTRAN = ['-fcheck=all', '-Og']
-F77_COMPILE_OPT_GFORTRAN = ['-Ofast','-march=native']
+F77_COMPILE_OPT_GFORTRAN = ['-O3', '-funroll-loops', '-march=native']
 F90_COMPILE_ARGS_GFORTRAN = ['-cpp']
 F90_COMPILE_DEBUG_GFORTRAN = ['-fcheck=all', '-Og']
-F90_COMPILE_OPT_GFORTRAN = ['-Ofast', '-march=native']
+F90_COMPILE_OPT_GFORTRAN = ['-O3', '-funroll-loops', '-march=native']
 F77_COMPILE_ARGS_IFORT = []
 F77_COMPILE_DEBUG_IFORT = ['-check all']
 F77_COMPILE_OPT_IFORT = ['-fast']
@@ -69,10 +70,29 @@ numpy.distutils.from_template.function_start_re = re.compile(
     r'\n     (\$|\*)\s*((im)?pure\s+|elemental\s+)*function\b', re.I)
 
 # monkey patch compilers
+# the debug flag as derived in FCompiler.customize is not reliable
+# we set everything in the subclasses of build_clib and build_ext
 Gnu95FCompiler.get_flags_debug = lambda self: []
+Gnu95FCompiler.get_flags_debug_f77 = lambda self: []
+Gnu95FCompiler.get_flags_debug_f90 = lambda self: []
 Gnu95FCompiler.get_flags_opt = lambda self: []
+Gnu95FCompiler.get_flags_opt_f77 = lambda self: []
+Gnu95FCompiler.get_flags_opt_f90 = lambda self: []
 IntelEM64TFCompiler.get_flags_debug = lambda self: []
+IntelEM64TFCompiler.get_flags_debug_f77 = lambda self: []
+IntelEM64TFCompiler.get_flags_debug_f90 = lambda self: []
 IntelEM64TFCompiler.get_flags_opt = lambda self: []
+IntelEM64TFCompiler.get_flags_opt_f77 = lambda self: []
+IntelEM64TFCompiler.get_flags_opt_f90 = lambda self: []
+
+# we unset the F*FLAGS environment variables, because they would be added to the
+# command before the build_libraries and build_extensions methods are called.
+FFLAGS = os.environ.get('FFLAGS', '').split()
+F77FLAGS = os.environ.get('F77FLAGS', '').split()
+F90FLAGS = os.environ.get('F90FLAGS', '').split()
+os.environ['FFLAGS'] = ''
+os.environ['F77FLAGS'] = ''
+os.environ['F90FLAGS'] = ''
 
 # monkey patch the default Fortran compiler
 if sys.platform.startswith('linux'):
@@ -93,39 +113,11 @@ class BuildClibCommand(build_clib):
         if fcompiler is None:
             raise ValueError('build_clib._f_compiler is None.')
 
-        if isinstance(fcompiler, numpy.distutils.fcompiler.gnu.Gnu95FCompiler):
-            flags = F77_COMPILE_ARGS_GFORTRAN + F77_COMPILE_OPT_GFORTRAN
-            if self.debug:
-                flags += F77_COMPILE_DEBUG_GFORTRAN
-            if F77_OPENMP:
-                flags += FLAGS_OPENMP['gfortran']
-            fcompiler.executables['compiler_f77'] += flags
-            flags = F90_COMPILE_ARGS_GFORTRAN + F90_COMPILE_OPT_GFORTRAN
-            if self.debug:
-                flags += F90_COMPILE_DEBUG_GFORTRAN
-            if F90_OPENMP:
-                flags += FLAGS_OPENMP['gfortran']
-            fcompiler.executables['compiler_f90'] += flags
-            fcompiler.libraries += [LIBRARY_OPENMP['gfortran']]
+        fcompiler_fixup(fcompiler, 'F77', self.debug)
+        fcompiler_fixup(fcompiler, 'F90', self.debug)
 
-        elif isinstance(fcompiler, numpy.distutils.fcompiler.intel.IntelFCompiler):
+        if isinstance(fcompiler, numpy.distutils.fcompiler.intel.IntelFCompiler):
             self.compiler.archiver[0] = find_executable('xiar')
-            flags = F77_COMPILE_ARGS_IFORT + F77_COMPILE_OPT_IFORT
-            if self.debug:
-                flags += F77_COMPILE_DEBUG_IFORT
-            if F77_OPENMP:
-                flags += FLAGS_OPENMP['ifort']
-            fcompiler.executables['compiler_f77'] += flags
-            flags = F90_COMPILE_ARGS_IFORT + F90_COMPILE_OPT_IFORT
-            if self.debug:
-                flags += F90_COMPILE_DEBUG_IFORT
-            if F90_OPENMP:
-                flags += FLAGS_OPENMP['ifort']
-            fcompiler.executables['compiler_f90'] += flags
-            fcompiler.libraries += [LIBRARY_OPENMP['ifort']]
-
-        elif fcompiler is not None:
-            raise RuntimeError("Unhandled compiler: '{}'.".format(fcompiler))
 
         try:
             super().build_libraries(libraries)
@@ -219,41 +211,8 @@ class BuildExtCommand(build_ext):
                 self.warn('f90_compiler=%s is not available.' % ctype)
 
         for fc in self._f77_compiler, self._f90_compiler:
-
-            if isinstance(fc, numpy.distutils.fcompiler.gnu.Gnu95FCompiler):
-                assert fc is not None
-                flags = F77_COMPILE_ARGS_GFORTRAN + F77_COMPILE_OPT_GFORTRAN
-                if self.debug:
-                    flags += F77_COMPILE_DEBUG_GFORTRAN
-                if F77_OPENMP:
-                    flags += FLAGS_OPENMP['gfortran']
-                fc.executables['compiler_f77'] += flags
-                flags = F90_COMPILE_ARGS_GFORTRAN + F90_COMPILE_OPT_GFORTRAN
-                if self.debug:
-                    flags += F90_COMPILE_DEBUG_GFORTRAN
-                if F90_OPENMP:
-                    flags += FLAGS_OPENMP['gfortran']
-                fc.executables['compiler_f90'] += flags
-                fc.libraries += [LIBRARY_OPENMP['gfortran']]
-
-            elif isinstance(fc, numpy.distutils.fcompiler.intel.IntelFCompiler):
-                assert fc is not None
-                flags = F77_COMPILE_ARGS_IFORT + F77_COMPILE_OPT_IFORT
-                if self.debug:
-                    flags += F77_COMPILE_DEBUG_IFORT
-                if F77_OPENMP:
-                    flags += FLAGS_OPENMP['ifort']
-                fc.executables['compiler_f77'] += flags
-                flags = F90_COMPILE_ARGS_IFORT + F90_COMPILE_OPT_IFORT
-                if self.debug:
-                    flags += F90_COMPILE_DEBUG_IFORT
-                if F90_OPENMP:
-                    flags += FLAGS_OPENMP['ifort']
-                fc.executables['compiler_f90'] += flags
-                fc.libraries += [LIBRARY_OPENMP['ifort']]
-
-            elif fc is not None:
-                raise RuntimeError(f"Unhandled compiler: '{fc}'.")
+            fcompiler_fixup(fc, 'F77', self.debug)
+            fcompiler_fixup(fc, 'F90', self.debug)
 
         try:
             super().build_extensions()
@@ -267,6 +226,30 @@ class BuildExtCommand(build_ext):
             print(f'_f90_compiler: {self._f90_compiler}')
             if self._f90_compiler:
                 print(f'_f90_compiler: {self._f90_compiler.executables}')
+
+
+def fcompiler_fixup(fc: FCompiler, fc_type: Literal['F77', 'F90'], debug: bool):
+    try:
+        fcompiler_name = {
+            numpy.distutils.fcompiler.gnu.Gnu95FCompiler: 'gfortran',
+            numpy.distutils.fcompiler.intel.IntelFCompiler: 'ifort',
+        }[type(fc)]
+    except KeyError:
+        raise RuntimeError(f"Unhandled compiler: '{fc}'.")
+
+    openmp = eval(f'{fc_type}_OPENMP')
+
+    flags = (
+        eval(f'{fc_type}_COMPILE_ARGS_{fcompiler_name.upper()}') +
+        eval(f'{fc_type}_COMPILE_OPT_{fcompiler_name.upper()}') +
+        (eval(f'{fc_type}_COMPILE_DEBUG_{fcompiler_name.upper()}') if debug else []) +
+        (FLAGS_OPENMP[fcompiler_name] if openmp else []) +
+        FFLAGS +
+        eval(f'{fc_type}FLAGS')
+    )
+    fc.executables[f'compiler_{fc_type.lower()}'] += flags
+    if openmp:
+        fc.libraries += [LIBRARY_OPENMP[fcompiler_name]]
 
 
 class BuildSrcCommand(build_src):
